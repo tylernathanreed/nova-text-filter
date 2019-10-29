54647b22230a103ca1003842fa1c693242d9037b
Tyler Reed <tyler.reed@tsico.com>
2019-10-29 16:26:20 -0500

    + Added service provider

---
+++ src/FilterServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace NovaComponents\SlideshowPlayer;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class FilterServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::serving(function(ServingNova $event) {
+            Nova::script('text-filter', __DIR__.'/../dist/js/text-filter.js');
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
